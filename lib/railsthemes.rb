ece04f45eb632f588ff172c5c9b85fbccdd2667d
Anthony Panozzo <panozzaj@gmail.com>
2012-02-22 07:59:02 -0500

    Add spec for read from file.

--- lib/railsthemes.rb
+++ lib/railsthemes.rb
@@ -2,5 +2,12 @@ require "railsthemes/version"
 
 module Railsthemes
   def self.install *args
+    if args[0] == '--file'
+      read_from_file args[1]
+    end
+  end
+
+  def self.read_from_file filepath
+    puts 'here'
   end
 end
--- spec/lib/railsthemes_spec.rb
+++ spec/lib/railsthemes_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
 
 describe Railsthemes do
-  describe 'install' do
+  describe :install do
+    it 'should take --file as a parameter and read from the file argument' do
+      mock(Railsthemes).read_from_file('abc')
+      Railsthemes.install '--file', 'abc'
+    end
+  end
+
+  describe :read_from_file do
   end
 end
