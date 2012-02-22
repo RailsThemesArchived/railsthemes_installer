ebaf4b6591cd8919418c979e2e4209db284a953b
Anthony Panozzo <panozzaj@gmail.com>
2012-02-22 08:11:03 -0500

    Add parameter error checking.

--- lib/railsthemes.rb
+++ lib/railsthemes.rb
@@ -3,14 +3,21 @@ require "railsthemes/version"
 module Railsthemes
   def self.install *args
     if args[0] == '--file'
-      read_from_file args[1]
+      if args[1]
+        read_from_file args[1]
+      else
+        log_and_abort("The parameter --file means we need another parameter after it to specify what file to load from.")
+      end
     else
       download_from_hash args[0]
     end
   end
 
+  def self.log_and_abort s
+    abort(s)
+  end
+
   def self.read_from_file filepath
-    # TODO
   end
 
   def self.download_from_hash hash
--- spec/lib/railsthemes_spec.rb
+++ spec/lib/railsthemes_spec.rb
@@ -2,14 +2,25 @@ require 'spec_helper'
 
 describe Railsthemes do
   describe :install do
-    it 'should take --file as a parameter and read from the file argument' do
-      mock(Railsthemes).read_from_file('filepath')
-      Railsthemes.install '--file', 'filepath'
+    context 'when --file is given as a parameter' do
+      it 'should read from the file argument' do
+        mock(Railsthemes).read_from_file('filepath')
+        Railsthemes.install '--file', 'filepath'
+      end
+
+      it 'should exit if filepath is nil' do
+        dont_allow(Railsthemes).read_from_file(anything)
+        dont_allow(Railsthemes).download_from_hash(anything)
+        stub(Railsthemes).log_and_abort(/parameter/)
+        Railsthemes.install '--file'
+      end
     end
 
-    it 'should otherwise just pass in the hash to the hash reading argument' do
-      mock(Railsthemes).download_from_hash('hash')
-      Railsthemes.install 'hash'
+    context 'otherwise' do
+      it 'should otherwise just pass in the hash to the hash reading argument' do
+        mock(Railsthemes).download_from_hash('hash')
+        Railsthemes.install 'hash'
+      end
     end
   end
 
