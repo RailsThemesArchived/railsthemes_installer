00b37110ddcb24c128b14177b9cff24c6ce49ec4
Anthony Panozzo <panozzaj@gmail.com>
2013-02-24 19:58:33 -0500

    Refactor switcher a bit.

--- lib/railsthemes/switcher.rb
+++ lib/railsthemes/switcher.rb
@@ -3,18 +3,24 @@ module Railsthemes
     include Railsthemes::Logging
 
     def list
-      dirs = Dir['app/assets/stylesheets/railsthemes_*'].inject([]) do |accum, filepath|
-        accum << filepath if File.directory?(filepath) ; accum
-      end
-
-      if dirs.empty?
+      themes = installed_themes
+      if themes.empty?
         Logging.logger.warn 'There are currently no RailsThemes themes installed.'
       else
         Logging.logger.warn 'RailsThemes themes currently installed:'
-        dirs.each do |dir|
-          theme_name = File.basename(dir).gsub(/^railsthemes_/, '')
-          Logging.logger.warn " - #{theme_name}"
+        themes.each do |theme|
+          Logging.logger.warn " - #{theme}"
         end
+      end
+    end
+
+    def switch_to
+    end
+
+    def installed_themes
+      Dir['app/assets/stylesheets/railsthemes_*'].inject([]) do |accum, filepath|
+        accum << File.basename(filepath).gsub(/^railsthemes_/, '') if File.directory?(filepath)
+        accum
       end
     end
   end
--- spec/lib/railsthemes/switcher_spec.rb
+++ spec/lib/railsthemes/switcher_spec.rb
@@ -8,41 +8,59 @@ describe Railsthemes::Switcher do
     @tempdir = stub_tempdir
   end
 
-  describe '.list' do
-    context 'there are no themes installed' do
-      it 'should show nothing installed ' do
+  describe '#list' do
+    context 'no themes are available' do
+      it 'should output no themes available' do
+        mock(@switcher).installed_themes { [] }
         mock(Railsthemes::Logging.logger).warn 'There are currently no RailsThemes themes installed.'
         @switcher.list
       end
     end
 
+    context 'themes are available' do
+      it 'should output the themes' do
+        mock(@switcher).installed_themes { ['theme1', 'theme2'] }
+        mock(Railsthemes::Logging.logger).warn 'RailsThemes themes currently installed:'
+        mock(Railsthemes::Logging.logger).warn ' - theme1'
+        mock(Railsthemes::Logging.logger).warn ' - theme2'
+        @switcher.list
+      end
+    end
+  end
+
+  describe '#switch_to' do
+  end
+
+  describe '#installed_themes' do
+    context 'there are no themes installed' do
+      it 'should show nothing installed ' do
+        @switcher.installed_themes.should == []
+      end
+    end
+
     context 'one theme installed' do
       before do
-        create_file('app/assets/stylesheets/railsthemes_themename.css')
-        create_file('app/assets/stylesheets/railsthemes_themename/something.css')
+        create_file('app/assets/stylesheets/railsthemes_theme1.css')
+        create_file('app/assets/stylesheets/railsthemes_theme1/something.css')
       end
 
       it 'should show the theme installed' do
-        mock(Railsthemes::Logging.logger).warn 'RailsThemes themes currently installed:'
-        mock(Railsthemes::Logging.logger).warn ' - themename'
-        @switcher.list
+        @switcher.installed_themes.should == ['theme1']
       end
     end
 
     context 'many themes installed' do
       before do
-        create_file('app/assets/stylesheets/railsthemes_themename.css')
-        create_file('app/assets/stylesheets/railsthemes_themename/something.css')
-        create_file('app/assets/stylesheets/railsthemes_themename2.css')
-        create_file('app/assets/stylesheets/railsthemes_themename2/something.css')
+        create_file('app/assets/stylesheets/railsthemes_theme1.css')
+        create_file('app/assets/stylesheets/railsthemes_theme1/something.css')
+        create_file('app/assets/stylesheets/railsthemes_theme2.css')
+        create_file('app/assets/stylesheets/railsthemes_theme2/something.css')
       end
 
       it 'should show the installed themes' do
-        mock(Railsthemes::Logging.logger).warn 'RailsThemes themes currently installed:'
-        mock(Railsthemes::Logging.logger).warn ' - themename'
-        mock(Railsthemes::Logging.logger).warn ' - themename2'
-        @switcher.list
+        @switcher.installed_themes.should == ['theme1', 'theme2']
       end
     end
   end
+
 end
