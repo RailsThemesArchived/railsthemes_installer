0dde385c3be2f2540e5edbec20f2aef865abff84
Anthony Panozzo <panozzaj@gmail.com>
2013-04-03 18:39:28 -0400

    Change premailer gem.

--- lib/railsthemes/email_installer.rb
+++ lib/railsthemes/email_installer.rb
@@ -6,7 +6,6 @@ module Railsthemes
       logger.warn 'Installing email...'
 
       install_mail_gems_if_necessary
-      add_premailer_config_file
 
       logger.warn 'Done installing email.'
     end
@@ -14,19 +13,13 @@ module Railsthemes
     def install_mail_gems_if_necessary
       gem_names = Utils.gemspecs.map(&:name)
       logger.debug "gem_names: #{gem_names}"
-      unless gem_names.include?('premailer-rails3')
+      unless gem_names.include?('premailer-rails')
         if (gem_names & ['hpricot', 'nokogiri']).empty?
           Utils.add_gem_to_gemfile 'hpricot'
         end
         logger.warn 'Installing assistant mail gems...'
-        Utils.add_gem_to_gemfile 'premailer-rails3'
+        Utils.add_gem_to_gemfile 'premailer-rails'
         logger.warn 'Done installing assistant mail gems.'
-      end
-    end
-
-    def add_premailer_config_file
-      Utils.safe_write('config/initializers/premailer.rb') do |f|
-        f.puts "PremailerRails.config.merge(:input_encoding => 'UTF-8', :generate_text_part => true)"
       end
     end
   end
--- spec/lib/railsthemes/email_installer_spec.rb
+++ spec/lib/railsthemes/email_installer_spec.rb
@@ -9,38 +9,29 @@ describe Railsthemes::EmailInstaller do
   end
 
   describe '#install_mail_gems_if_necessary' do
-    it 'should install no new gems if premailer-rails3 gem already installed' do
-      write_gemfiles_using_gems 'premailer-rails3', 'hpricot'
+    it 'should install no new gems if premailer-rails gem already installed' do
+      write_gemfiles_using_gems 'premailer-rails', 'hpricot'
       dont_allow(Railsthemes::Utils).add_gem_to_gemfile(anything)
       @installer.install_mail_gems_if_necessary
     end
 
-    it 'when nokogiri already installed, should install the pr3 gem' do
+    it 'when nokogiri already installed, should install the pr gem' do
       write_gemfiles_using_gems 'nokogiri'
-      mock(Railsthemes::Utils).add_gem_to_gemfile('premailer-rails3')
+      mock(Railsthemes::Utils).add_gem_to_gemfile('premailer-rails')
       @installer.install_mail_gems_if_necessary
     end
 
-    it 'when hpricot already installed, should install the pr3 gem only' do
+    it 'when hpricot already installed, should install the pr gem only' do
       write_gemfiles_using_gems 'hpricot'
-      mock(Railsthemes::Utils).add_gem_to_gemfile('premailer-rails3')
+      mock(Railsthemes::Utils).add_gem_to_gemfile('premailer-rails')
       @installer.install_mail_gems_if_necessary
     end
 
-    it 'when no xml gem or pr3 installed, should install the pr3 gem and hpricot' do
+    it 'when no xml gem or pr installed, should install the pr gem and hpricot' do
       FileUtils.touch('Gemfile.lock')
       mock(Railsthemes::Utils).add_gem_to_gemfile('hpricot')
-      mock(Railsthemes::Utils).add_gem_to_gemfile('premailer-rails3')
+      mock(Railsthemes::Utils).add_gem_to_gemfile('premailer-rails')
       @installer.install_mail_gems_if_necessary
-    end
-  end
-
-  describe '#add_premailer_config_file' do
-    it 'should add the file' do
-      @installer.add_premailer_config_file
-      filename = 'config/initializers/premailer.rb'
-      File.should exist(filename)
-      File.read(filename).split("\n").grep(/PremailerRails.config/).count.should eq(1)
     end
   end
 end
