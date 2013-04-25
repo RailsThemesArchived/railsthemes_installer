166d7a1113301a70053638f4b3a912fb85d8534e
Anthony Panozzo <panozzaj@gmail.com>
2013-04-25 03:02:40 -0400

    Clean up email installer slightly.

--- lib/railsthemes/email_installer.rb
+++ lib/railsthemes/email_installer.rb
@@ -2,9 +2,13 @@ module Railsthemes
   class EmailInstaller
     include Railsthemes::Logging
 
+    def email_stylesheet_filenames theme_name
+      Dir["app/assets/stylesheets/railsthemes_#{theme_name}/*_email.css.*"]
+    end
+
     def install_from_file_system source_filepath
       theme_name = Utils.read_file(File.join(source_filepath, 'theme_name')).chomp
-      if Dir["app/mailers/railsthemes_#{theme_name}/*"].count > 0
+      if email_stylesheet_filenames(theme_name).count > 0
         logger.warn 'Installing email...'
         logger.info "Source filepath: #{source_filepath}"
 
@@ -23,13 +27,13 @@ module Railsthemes
     end
 
     def add_to_asset_precompilation_list theme_name
-      filenames = Dir["app/assets/stylesheets/railsthemes_#{theme_name}/*_email.css*"]
-      filenames.map! do |filename|
+      filenames = email_stylesheet_filenames(theme_name).map do |filename|
         "railsthemes_#{theme_name}/#{File.basename(filename.gsub(/\.erb$/, ''))}"
       end
+      updated_or_new_line = "  config.assets.precompile += %w( #{filenames.join(' ')} )"
 
       config_lines = Utils.lines('config/environments/production.rb')
-      email_regex = /^\s*config.assets.precompile\s*\+=\s*%w\(\s*railsthemes_#{theme_name}\/\w*email\.css \)$/
+      email_regex = /^\s*config.assets.precompile\s*\+=\s*%w\(\s*railsthemes_#{theme_name}\/\w*email\.css.*\)$/
       count = config_lines.grep(email_regex).count
       if count == 0 # precompile line we want not found, add it
         added = false # only want to add the new line once
@@ -37,10 +41,7 @@ module Railsthemes
           config_lines.each do |line|
             f.puts line
             if !added && (line =~ /Precompile additional assets/ || line =~ /config\.assets\.precompile/)
-              string = '  config.assets.precompile += %w( '
-              string += filenames.join(' ')
-              string += ' )'
-              f.puts string
+              f.puts updated_or_new_line
               added = true
             end
           end
@@ -49,10 +50,7 @@ module Railsthemes
         Utils.safe_write('config/environments/production.rb') do |f|
           config_lines.each do |line|
             if line =~ email_regex
-              string = '  config.assets.precompile += %w( '
-              string += filenames.join(' ')
-              string += ' )'
-              f.puts string
+              f.puts updated_or_new_line
             else
               f.puts line
             end
